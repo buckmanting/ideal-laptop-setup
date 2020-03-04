e767966e743eae2e9ec42cd2202a50800dc5ddda
Aaron Buckley <32135001+springerBuck@users.noreply.github.com>
2020-03-04 14:28:23 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,5 @@
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 brew doctor
 // install all other tools, following tutorials
+read -p 'GitHub email: ' emailvar
+ssh-keygen -t rsa -b 4096 -C $emailvar
